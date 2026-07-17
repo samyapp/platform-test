20febddd07df354683d6b765b2fa60327370332f
Sam Yapp <sam@samyapp.com>
2026-07-17 17:54:25 +0100

    Initialize database connection in index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+$connectionString = 'mysql://root:yZYNlSQHVdShClomToapqZHLw24Z6yIy@mysql:3306/app';
+
+$conn = mysqli_connect('mysql', 'root', 'yZYNlSQHVdShClomToapqZHLw24Z6yIy', 'app') or die(mysqli_error());
+
