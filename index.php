5d2fab455d8147ecc8e75f55f1ae448600d4f6ab
Sam Yapp <sam@samyapp.com>
2026-07-18 21:38:18 +0100

    Implement database connection status messages
    Add connection success/error messages to index.php

--- index.php
+++ index.php
@@ -4,3 +4,10 @@ $connectionString = 'mysql://root:yZYNlSQHVdShClomToapqZHLw24Z6yIy@mysql:3306/ap
 
 $conn = mysqli_connect('mysql', 'root', 'yZYNlSQHVdShClomToapqZHLw24Z6yIy', 'app') or die(mysqli_error());
 
+if ($conn) {
+echo '<h1>SUCCESS</h1>';
+} else {
+echo '<h1>ERROR</h1>';
+}
+
+phpinfo();
